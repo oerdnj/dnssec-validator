d98184dfbbc0337498667bc57164a3fe82448eba
Ondřej Surý <ondrej@sury.org>
2014-07-22 00:38:16 +0200

    Remove extra dh_make cruft from debian/ directory

--- debian/dnssec-validator.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the dnssec-validator package
-#
-0 4	* * *	root	[ -x /usr/bin/dnssec-validator_maintenance ] && /usr/bin/dnssec-validator_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(dnssec-validator):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="dnssec-validator" command="/usr/bin/dnssec-validator"
